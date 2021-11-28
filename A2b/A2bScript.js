1493ad86305af469e6753804617bf829337f3f7d
BloodFrost <55624580+activatedbonkers@users.noreply.github.com>
2021-11-29 03:30:30 +0530

    Add files via upload
    Create an HTML Page with Javascript event handling that will: 
    Changes the background colour of a text box when in focus and out of focus

---
+++ A2b/A2bScript.js
@@ -0,0 +1,9 @@
+// Focus = Changes the background color of input to yellow
+function focusFunction() {
+    document.getElementById("myInput").style.background = "yellow";
+  }
+  
+  // No focus = Changes the background color of input to red
+  function blurFunction() {
+    document.getElementById("myInput").style.background = "red";
+  }
