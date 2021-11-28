ff30e05758310a7a6bb90b51db1ef785ced6db74
BloodFrost <55624580+activatedbonkers@users.noreply.github.com>
2021-11-29 03:26:25 +0530

    Delete A2 directory

--- A2/main.js
+++
@@ -1,6 +0,0 @@
-let tp=0
-document.getElementById("sbutton").addEventListener("click",function(){
-    tp+=document.getElementById("price").value*document.getElementById("number").value
-        document.getElementById("cost").innerHTML=tp
-        console.log(tp)
-}) 
